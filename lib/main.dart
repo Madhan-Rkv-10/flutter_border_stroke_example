a80454a54b7cd58560337fc313606289b5ba2bd6
Madhan-Rkv-10 <madhanrkv10@gmail.com>
2025-05-31 16:19:33 +0530

    ✨ feat: add border stroke example

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,281 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+/// Flutter code sample for [BorderSide.strokeAlign].
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() => runApp(const StrokeAlignApp());
 
-  // This widget is the root of your application.
+class StrokeAlignApp extends StatelessWidget {
+  const StrokeAlignApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: StrokeAlignExample(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class StrokeAlignExample extends StatefulWidget {
+  const StrokeAlignExample({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StrokeAlignExample> createState() => _StrokeAlignExampleState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _StrokeAlignExampleState extends State<StrokeAlignExample>
+    with TickerProviderStateMixin {
+  late final AnimationController animation;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    animation = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    animation.repeat(reverse: true);
+    animation.addListener(_markDirty);
   }
 
   @override
+  void dispose() {
+    animation.dispose();
+    super.dispose();
+  }
+
+  void _markDirty() {
+    setState(() {});
+  }
+
+  static const double borderWidth = 10;
+  static const double cornerRadius = 10;
+  static const Color borderColor = Color(0x8000b4fc);
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      appBar: AppBar(title: const Text("BorderSide Stroke Examples")),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              const Text(
+                "StadiumBorder",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              BorderedBox(
+                shape: StadiumBorder(
+                  side: BorderSide(
+                    color: borderColor,
+                    width: borderWidth,
+                    strokeAlign: (animation.value * 2) - 1,
+                  ),
+                ),
+              ),
+
+              const Divider(),
+              const Text(
+                "CircleBorder & OvalBorder",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Column(
+                    children: [
+                      const Text("CircleBorder"),
+                      BorderedBox(
+                        shape: CircleBorder(
+                          side: BorderSide(
+                            color: borderColor,
+                            width: borderWidth,
+                            strokeAlign: (animation.value * 2) - 1,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      const Text("OvalBorder"),
+                      BorderedBox(
+                        shape: OvalBorder(
+                          side: BorderSide(
+                            color: borderColor,
+                            width: borderWidth,
+                            strokeAlign: (animation.value * 2) - 1,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
+              const Divider(),
+              const Text(
+                "BeveledRectangleBorder",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Column(
+                    children: [
+                      const Text("Default Bevel"),
+                      BorderedBox(
+                        shape: BeveledRectangleBorder(
+                          side: BorderSide(
+                            color: borderColor,
+                            width: borderWidth,
+                            strokeAlign: (animation.value * 2) - 1,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      const Text("Rounded Bevel"),
+                      BorderedBox(
+                        shape: BeveledRectangleBorder(
+                          borderRadius: BorderRadius.circular(cornerRadius),
+                          side: BorderSide(
+                            color: borderColor,
+                            width: borderWidth,
+                            strokeAlign: (animation.value * 2) - 1,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
+              const Divider(),
+              const Text(
+                "RoundedRectangleBorder",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Column(
+                    children: [
+                      const Text("Default Rectangle"),
+                      BorderedBox(
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                            color: borderColor,
+                            width: borderWidth,
+                            strokeAlign: (animation.value * 2) - 1,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      const Text("Rounded Rectangle"),
+                      BorderedBox(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(cornerRadius),
+                          side: BorderSide(
+                            color: borderColor,
+                            width: borderWidth,
+                            strokeAlign: (animation.value * 2) - 1,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
+              const Divider(),
+              const Text(
+                "StarBorder Variations",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Column(
+                    children: [
+                      const Text("Standard Star"),
+                      BorderedBox(
+                        shape: StarBorder(
+                          side: BorderSide(
+                            color: borderColor,
+                            width: borderWidth,
+                            strokeAlign: (animation.value * 2) - 1,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      const Text("8-Point Star"),
+                      BorderedBox(
+                        shape: StarBorder(
+                          pointRounding: 1,
+                          innerRadiusRatio: 0.5,
+                          points: 8,
+                          side: BorderSide(
+                            color: borderColor,
+                            width: borderWidth,
+                            strokeAlign: (animation.value * 2) - 1,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      const Text("6-Side Polygon"),
+                      BorderedBox(
+                        shape: StarBorder.polygon(
+                          sides: 6,
+                          pointRounding: 0.5,
+                          side: BorderSide(
+                            color: borderColor,
+                            width: borderWidth,
+                            strokeAlign: (animation.value * 2) - 1,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class BorderedBox extends StatelessWidget {
+  const BorderedBox({super.key, required this.shape});
+
+  final ShapeBorder shape;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 100,
+      height: 50,
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 8),
+        decoration: ShapeDecoration(
+          color: const Color(0xff012677),
+          shape: shape,
+        ),
+      ),
     );
   }
 }
